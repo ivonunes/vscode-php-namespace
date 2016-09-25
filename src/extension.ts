21aeb43d317b9acb2589ea607f05a63cfd692f68
Ivo Nunes <ivonunes@me.com>
2016-09-25 02:06:03 +0100

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2016 Ivo Nunes <ivonunes@me.com>
+ *
+ * This software is licensed under the GNU General Public License
+ * (version 3 or later). See the COPYING file in this distribution.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this software; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+'use strict';
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let resolveNamespaceCommand = vscode.commands.registerCommand('extension.resolveNamespace', resolveNamespace);
+    context.subscriptions.push(resolveNamespaceCommand);
+}
+
+function isCandidate(text: string) {
+    return text.startsWith('<?php')
+        || text.startsWith('namespace ')
+        || text.startsWith('use ');
+}
+
+function insertNamespace(editor: vscode.TextEditor, namespace: string) {
+    editor.edit(textEdit => {
+        let insertAt = null;
+        let prependText = '\n';
+
+        for (let line = 0; line < editor.document.lineCount; line++) {
+            let text = editor.document.lineAt(line).text;
+            
+            if (isCandidate(text)) {
+                insertAt = line + 1;
+
+                if (text.startsWith('use ')) {
+                    prependText = '';
+                }
+            }
+        }
+
+        if (insertAt != null) {
+            textEdit.replace(new vscode.Position(insertAt, 0), prependText + 'use ' + namespace + ';\n');
+        }
+    });
+}
+
+function resolveNamespace() {
+    let editor = vscode.window.activeTextEditor;
+    let document = editor.document;
+    let selections = editor.selections;
+
+    for (let selection of selections) {
+        if (selection.isEmpty) {
+            vscode.window.showErrorMessage('Please select a class name.');
+        } else {
+            const selectedClass = editor.document.getText(selection);
+            let searchExts = '**/*.php';
+            let excludedFiles = '**/node_modules/**';
+            let namespaces = [];
+            
+            vscode.workspace.findFiles(searchExts, excludedFiles).then(files => {
+                if (files) {
+                    files.forEach((file, index, array) => {
+                        let fileName = file.fsPath
+                            .replace(/^.*[\\\/]/, '')
+                            .split('.')[0];
+
+                        if (fileName == selectedClass) {
+                            vscode.workspace.openTextDocument(file).then(doc => {
+                                for (let line = 0; line < doc.lineCount; line++) {
+                                    let textLine = doc.lineAt(line).text;
+
+                                    if (textLine.startsWith('namespace ') || textLine.startsWith('<?php namespace ')) {
+                                        let namespace = textLine
+                                            .split('namespace ')[1]
+                                            .split(';')[0]
+                                            + '\\'
+                                            + selectedClass;
+
+                                        if (namespaces.indexOf(namespace) == -1) {
+                                            namespaces.push(namespace);
+                                            insertNamespace(editor, namespace);
+                                        }
+
+                                        break;
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
