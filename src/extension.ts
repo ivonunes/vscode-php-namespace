40e246739116265fd6e4e12cbf6bad03a237dc2a
Ivo Nunes <ivonunes@me.com>
2019-01-13 17:19:06 +0000

    Relicense as MIT

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,3 @@
-/*
- * Copyright (C) 2016 Ivo Nunes <ivonunes@me.com>
- *
- * This software is licensed under the GNU General Public License
- * (version 3 or later). See the COPYING file in this distribution.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this software; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
 'use strict';
 
 import * as vscode from 'vscode';
